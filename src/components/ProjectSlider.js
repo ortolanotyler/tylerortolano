8baefe881b1dd221090eea2080425809d00ba139
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-07-30 16:32:47 -0400

    43

--- src/components/ProjectSlider.js
+++ src/components/ProjectSlider.js
@@ -4,35 +4,14 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import { styled } from '@mui/system';
 import theme from '../styles/theme';
 
-const muiTheme = createTheme({
-  palette: {
-    primary: {
-      main: theme.colors.bitcoinOrange,
-    },
-    secondary: {
-      main: theme.colors.secondary,
-    },
-    text: {
-      primary: theme.colors.text,
-      secondary: theme.colors.secondary,
-    },
-    background: {
-      paper: theme.colors.background,
-    },
-  },
-  typography: {
-    fontFamily: 'Open Sans, sans-serif',
-  },
-});
-
 const projects = [
   {
-    title: 'JackieWyers.Beauty',
-    description: 'Description for project 1',
+    title: 'JackieWyers.beauty',
+    description: "For my very first project, I developed JackieWyers.beauty using ReactJS, ensuring the site could grow with the client's evolving needs, including the incorporation of various mini-applications. I used PostgreSQL for secure data management and Styled Components for scalable design. To optimize SEO, I integrated React-Helmet. This project provided valuable experience in translating client requirements into technical implementations and iterating based on user feedback.",
     image: `${process.env.PUBLIC_URL}/Images/jackiewyersbeauty.png`,
   },
   {
@@ -45,20 +24,20 @@ const projects = [
 
 const ProjectSlider = () => {
   return (
-    <ThemeProvider theme={muiTheme}>
+    <ThemeProvider theme={theme.muiTheme}>
       <SliderContainer>
         {projects.map((project, index) => (
           <StyledCard key={index}>
-            <CardContent>
+            <CardContentWrapper>
               <Title>{project.title}</Title>
               <HeadshotContainer>
                 <Headshot src={project.image} alt={project.title} />
               </HeadshotContainer>
               <Description>{project.description}</Description>
               <StyledButton variant="contained" color="primary">
-                Learn More
+                Visit
               </StyledButton>
-            </CardContent>
+            </CardContentWrapper>
           </StyledCard>
         ))}
       </SliderContainer>
@@ -75,47 +54,57 @@ const SliderContainer = styled('div')(({ theme }) => ({
 }));
 
 const StyledCard = styled(Card)(({ theme }) => ({
+  width: '100%',
   maxWidth: '600px',
-  width: '100%', // Ensure cards take full width of the container
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   boxShadow: 'none',
   border: 'none',
-  textAlign: 'center', // Center contents within the card
-  padding: '20px',
+}));
+
+const CardContentWrapper = styled(CardContent)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 }));
 
 const Title = styled(Typography)(({ theme }) => ({
   fontSize: '1.5rem',
-  fontWeight: 'bold',
+  fontWeight: 'normal',
   color: theme.palette.text.primary,
   marginBottom: '1rem',
+  textAlign: 'center',
 }));
 
 const Description = styled(Typography)(({ theme }) => ({
   fontSize: '1rem',
   color: theme.palette.text.secondary,
   marginBottom: '1.5rem',
+  textAlign: 'center',
 }));
 
 const HeadshotContainer = styled('div')`
-  display: flex;
-  justify-content: center;
-  width: 100%;
+  margin: 1.5rem;
 `;
 
 const Headshot = styled('img')(({ theme }) => ({
-  width: '250px',
-  height: '250px',
+  width: '300px',
+  height: '300px',
   borderRadius: '50%',
   objectFit: 'cover',
+  border: '5px solid black',
   boxShadow: '0.1rem 0.1rem 1rem rgba(0, 0, 0, 0.5)',
   marginBottom: '1rem',
+  '@media (max-width: 768px)': {
+    width: '300px',
+    height: '300px',
+  },
 }));
 
 const StyledButton = styled(Button)(({ theme }) => ({
   borderRadius: '50px',
+  marginTop: '1rem',
 }));
 
 export default ProjectSlider;
